858e88c686ee6a89a1e18b2b9304fa8ce24ff560
evecalm.mi <evecalm@live.com>
2013-09-11 18:31:05 +0800

    add htmlEncode & htmlDecode

--- curry.js
+++ curry.js
@@ -68,3 +68,13 @@ function extendDeep (parent, child) {
 	}
 	return child;
 }
+
+function htmlEncode( html ) {
+    return document.createElement( 'a' ).appendChild( 
+        document.createTextNode( html ) ).parentNode.innerHTML;
+}
+
+function htmlDecode( html ) {
+    var a = document.createElement( 'a' ); a.innerHTML = html;
+    return ('textContent' in a) ? a.textContent : a.innerText;
+}
