78d837fe559fa42e33fb8c267cb36489d56c66a7
evecalm.mi <evecalm@live.com>
2014-04-11 18:09:54 +0800

    add snippets

--- nodejs/rename.js
+++ nodejs/rename.js
@@ -1,40 +1,81 @@
 var path = require("path"),
 	fs = require("fs"),
-	rootPath = __filename,
+	// rootPath = path.dirname(__filename),
 	scriptName = path.basename(__filename);
 
-renameFilesInDir(path.dirname(rootPath));
+// traveldir(args[0] || rootPath, rename);
 
-function changeFileName(filepath){
-	fs.stat(filepath,function(err,stats){
-		if(stats.isFile()){
-			var filename = path.basename(filepath),
-				parentDir =path.dirname(filepath),
-				newName,newPath;
-			//console.log(thisFilename);
-			//这个if就是进行更改文件名的逻辑,可以自行定义,这里定义为将文件命名为当前文件夹的名字加"-文件自身名"
-			if( filename != scriptName ){
-				newName = filename.replace('幻灯片','').toLowerCase();
-				newPath = parentDir + path.sep + newName;
-				console.log("going to rename from "+filepath+" to "+newPath);
-				fs.rename(filepath,newPath);
-			}
-		}
-		else if(stats.isDirectory()){
-			console.log("============["+filepath+"] isDir===========");
-			renameFilesInDir(filepath);
-		}else{
-		console.log("unknow type of file");
+function traveldir (dir,callback) {
+	fs.readdir(dir, function (err,files) {
+		if (err) {
+			console.log('read file error' + error.message);
+		} else {
+			files.sort(function(a, b) {
+				return a < b ? -1 : 1;
+			}).forEach(function (file,key) {
+				var filePath = path.join(dir,file),
+					stat = fs.statSync(filePath);
+				if (stat.isFile()) {
+					callback(filePath);
+				} else if(stat.isDirectory()) {
+					traveldir(filePath, callback);
+				} else {
+					console.log('unknow file type:' + filePath);
+				}
+			});
 		}
 	});
 }
 
-function renameFilesInDir(dir){
-	fs.readdir(dir,function(error,files){
-		files.sort(function(a, b) {
-			return a < b ? -1 : 1;
-		}).forEach(function(file, key) {
-			changeFileName(dir+"\\"+file);
-		});
-	});
+function rename2LowerCase (filePath) {
+	var fileName = path.basename(filePath),
+		dirName = path.dirname(filePath),
+		newPath;
+	if (fileName !== scriptName) {
+		newPath = dirName + path.sep + fileName.toLowerCase();
+		console.log('rename from ' + filePath + ' to ' + newPath);
+		fs.rename(filePath, newPath);
+	}
+}
+
+function renameRemove (filePath, str, lowCase) {
+	var fileName = path.basename(filePath),
+		dirName = path.dirname(filePath),
+		newPath;
+	if (fileName !== scriptName) {
+		newPath = dirName + path.sep +
+			(lowCase ? fileName.replace(str,'').toLowerCase() : fileName.replace(str,''));
+
+		console.log('rename from ' + filePath + ' to ' + newPath);
+		fs.rename(filePath, newPath);
+	}
+}
+
+function main () {
+	var args = process.argv.slice(2),
+		dir = args[0],
+		type = args[1],
+		param = args[2],
+		tmp;
+	if (dir && fs.statSync(dir).isDirectory()) {
+		if (type.match(/^-/)) {
+			if (type.match(/r/i)) {
+				expression
+			} else if (type.match(/l/i)) {
+				expression
+			} else {
+				help();
+			}
+		} else {
+			help();
+		}
+	} else {
+		param = type;
+		type = dir;
+		dir = path.dirname(__filename);
+	}
+}
+
+function help () {
+	console.log('usage to do...');
 }
