4b446e9f2a7860afefb2f5b29fe9f491552fc7f8
Saiya <evecalm@Live.com>
2018-07-03 16:37:58 +0800

    support download https url(even with invalid cert)

--- nodejs/download.js
+++ nodejs/download.js
@@ -1,45 +1,56 @@
 const http = require('http')
+const https = require('https')
 const fs = require('fs')
 const url = require('url')
 const path = require('path')
 
-// 监听代码中未捕获的异常
+// break on all unhandled promise rejection
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
   process.exit(1)
 })
 
-// 下载超时时间
+// download timeout 10s
 const TIMEOUT = 10 * 1000
 async function download (options) {
   const { progress } = options
   const reqOptions = url.parse(options.url)
   const dest = options.dest
   const fileStream = fs.createWriteStream(dest)
-  const request = http.get(reqOptions)
+
+  let request
+  // support https
+  if (reqOptions.protocol === 'https:') {
+    // allow invalid (self assigned) cert
+    reqOptions.rejectUnauthorized = false
+    reqOptions.requestCert = true
+    request = https.get(reqOptions)
+  } else {
+    request = http.get(reqOptions)
+  }
   let hasFailed
   return new Promise((resolve, reject) => {
-    const onReqError = (err) => {
+    const onReqError = err => {
       hasFailed = true
       fileStream.end()
       fs.unlinkSync(dest)
       reject(err)
     }
-    // 监听处理连接超时情况
-    request.on('socket', (s) => {
+    // handle timeout
+    request.on('socket', s => {
       s.setTimeout(TIMEOUT, () => {
         onReqError(new Error('network timeout'))
         s.destroy()
       })
     })
-    request.on('response', (response) => {
+    request.on('response', response => {
       const totalBytes = parseInt(response.headers['content-length'], 10)
       let downloaded = 0
 
-      response.on('data', (chunk) => {
+      response.on('data', chunk => {
         fileStream.write(chunk)
         downloaded += chunk.length
-        progress && progress({totalBytes, downloaded, time: Date.now()})
+        progress && progress({ totalBytes, downloaded, time: Date.now() })
       })
 
       response.on('end', () => {
@@ -55,13 +66,17 @@ async function download (options) {
 }
 
 download({
-  url: 'http://img.ithome.com/newsuploadfiles/2018/2/20180201_121223_433.jpg',
+  // url: 'HTTP://img.ithome.com/newsuploadfiles/2018/2/20180201_121223_433.jpg',
+  url: 'https://ww1.sinaimg.cn/large/0065oQSqly1fswhaqvnobj30sg14hka0.jpg',
   dest: path.join(__dirname, 'abc.jpg'),
   progress (ob) {
     console.log('progress', ob)
   }
-}).then((resp) => {
-  console.log('done', resp)
-}, (err) => {
-  console.log('failed', err)
-})
+}).then(
+  resp => {
+    console.log('done', resp)
+  },
+  err => {
+    console.log('failed', err)
+  }
+)
